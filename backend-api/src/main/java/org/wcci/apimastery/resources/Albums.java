2774f00fe7648c6bc1be4158ea6eea7e9733f755
Kolton Koester <koltonkoester10@gmail.com>
2020-11-10 14:56:55 -0500

    finished the pojos for albums and songs

---
+++ backend-api/src/main/java/org/wcci/apimastery/resources/Albums.java
@@ -0,0 +1,69 @@
+package org.wcci.apimastery.resources;
+
+import java.util.Objects;
+
+public class Albums {
+    private String title;
+    private String image;
+    private String songs;
+    private String recordLabel;
+    private String comments;
+    private String ratings;
+
+    protected Albums(){
+
+    }
+
+    public Albums(String title, String image, String songs,String recordLabel, String comments,String ratings){
+
+
+        this.title = title;
+        this.image = image;
+        this.songs = songs;
+        this.recordLabel = recordLabel;
+        this.comments = comments;
+        this.ratings = ratings;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getSongs() {
+        return songs;
+    }
+
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public String getRatings() {
+        return ratings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Albums albums = (Albums) o;
+        return Objects.equals(title, albums.title) &&
+                Objects.equals(image, albums.image) &&
+                Objects.equals(songs, albums.songs) &&
+                Objects.equals(recordLabel, albums.recordLabel) &&
+                Objects.equals(comments, albums.comments) &&
+                Objects.equals(ratings, albums.ratings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, image, songs, recordLabel, comments, ratings);
+    }
+}
---
+++ backend-api/src/main/java/org/wcci/apimastery/resources/Songs.java
@@ -0,0 +1,59 @@
+package org.wcci.apimastery.resources;
+
+import java.util.Objects;
+
+public class Songs {
+    private long id;
+    private String title;
+    private String link;
+    private String duration;
+    private String comments;
+    private String ratings;
+
+    protected Songs() {
+    }
+    public Songs(long id, String title, String link, String duration, String comments, String ratings) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.duration = duration;
+        this.comments = comments;
+        this.ratings = ratings;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getLink() {
+        return link;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public String getRatings() {
+        return ratings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Songs songs = (Songs) o;
+        return id == songs.id &&
+                Objects.equals(title, songs.title) &&
+                Objects.equals(link, songs.link) &&
+                Objects.equals(duration, songs.duration) &&
+                Objects.equals(comments, songs.comments) &&
+                Objects.equals(ratings, songs.ratings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, link, duration, comments, ratings);
+    }
+}
