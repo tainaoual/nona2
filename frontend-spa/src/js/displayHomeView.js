6b84bc95994becf99386d87b14e782575c8664fd
Ryan Archer <ryanarcheryeah@gmail.com>
2020-11-17 15:03:26 -0500

    working on displaying all the albums on index.html

---
+++ frontend-spa/src/js/displayHomeView.js
@@ -0,0 +1,25 @@
+const displayHomeView = function(Albums){
+    const mainElement = document.createElement("section");
+    mainElement.classList.add("column-main")
+    const sectionElement = document.createElement("div")
+    sectionElement.classList.add("all-cards")
+    mainElement.appendChild(sectionElement);
+
+    let albumListHTML = "";
+
+    Albums.forEach(Album => {
+        albumListHTML += `
+            <div class="card">
+                <img src="./spec/Img/John-Doe.jpg" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <h4><b>${Album.title}</b></h4>
+                    <p>${Album.artist}</p>
+                </div>
+            </div>
+        `
+    });
+    sectionElement.innerHTML = albumListHTML;
+
+    return mainElement;
+}
+    export{displayHomeView}
