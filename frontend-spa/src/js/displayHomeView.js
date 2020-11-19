4aa0616528015247ae29849c273f82c6d980da37
Kolton Koester <koltonkoester10@gmail.com>
2020-11-19 14:24:36 -0500

    working on getting a single album to display with songs

--- frontend-spa/src/js/displayHomeView.js
+++ frontend-spa/src/js/displayHomeView.js
@@ -1,45 +1,46 @@
 import{
-    displaySingleAlbum
+    displaySingleAlbum,
+    clearChildren
 }from "./displaySingleAlbum.js"
 
-const displayHomeView = function(Albums){
+const displayHomeView = function(Album){
+
     const mainElement = document.createElement("main");
     mainElement.classList.add("main")
-    const columnMainElement =document.createElement("section");
+    
+    const columnMainElement = document.createElement("section");
     columnMainElement.classList.add("column-main")
+    
     const allCardElement = document.createElement("div");
     allCardElement.classList.add("all-cards")
+    
     const sectionElement = document.createElement("div")
     sectionElement.classList.add("card")
+
     mainElement.appendChild(columnMainElement);
     columnMainElement.appendChild(allCardElement);
     allCardElement.appendChild(sectionElement);
+   
 
-    let albumListHTML = "";
-
-    Albums.forEach(Album => {
+    Album.forEach(Album => {
         let albumElement = document.createElement('div');
         albumElement.classList.add("card");
         let albumTitleElement = document.createElement('h4');
         albumTitleElement.classList.add("albumTitle");
         albumTitleElement.innerText = Album.title;
-        albumTitleElement.addEventListener("click",()=> displaySingleAlbum(Album));
+        albumTitleElement.addEventListener("click",()=> displaySingleAlbum(album));
         let albumArtistElement = document.createElement("p");
         albumArtistElement.classList.add("albumArtist");
+        let albumImage =document.createElement('img')
+        albumImage.classList.add('album-image')
         albumArtistElement.innerText = Album.artist;
+        albumElement.appendChild(albumTitleElement);
+        albumElement.appendChild(albumArtistElement);
+        sectionElement.appendChild(albumElement);
+        sectionElement.appendChild(albumImage);
 
-
-        // albumListHTML += `
-        //     <div class="card">
-        //         <img src="./spec/Img/John-Doe.jpg" alt="Avatar" style="width:100%">
-        //         <div class="container">
-        //             <h4 class="albumTitle"><b>${Album.title}</b></h4>
-        //             <p class="albumArtist">${Album.artist}</p>
-        //         </div>
-        //     </div>
-        // `
     });
-    sectionElement.innerHTML = albumListHTML;
+    
 
     return mainElement;
 }
--- frontend-spa/src/js/displaySingleAlbum.js
+++ frontend-spa/src/js/displaySingleAlbum.js
@@ -3,13 +3,83 @@ import{
 } from "./displayHomeView.js"
 
 const displaySingleAlbum = function (Album) {
-    const mainAlbumElement = document.querySelector("")
+    const mainAlbumElement = document.querySelector(".main-album")
     clearChildren(mainAlbumElement);
     const singleAlbumElement = document.createElement("div");
     singleAlbumElement.classList.add("single-album");
     mainAlbumElement.appendChild(singleAlbumElement);
+
+    const albumCard = document.createElement('div');
+    
+    const albumNameElement = document.createElement("h4")
+    albumNameElement.innerText = Album.title;
+    albumNameElement.classList.add("albumTitle");
+    
+    const albumArtistElement = document.createElement("p");
+    albumArtistElement.innerText = Album.artist;
+    albumArtistElement.classList.add("albumArtist")
+    
+    const albumRatingElement = document.createElement("p")
+    albumRatingElement.innerText = Album.rating;
+    albumRatingElement.classList.add("albumRating")
+   
+    albumCard.appendChild(albumNameElement);
+    albumCard.appendChild(albumArtistElement);
+    albumCard.appendChild(albumRatingElement);
+    singleAlbumElement.appendChild(albumCard);
+
+    const songsRowElement = document.createElement("div");
+    songsRowElement.classList.add("row song-row");
+    singleAlbumElement.appendChild(songsRowElement);
+
+    Album.song.forEach(song => {
+        const songTitle = document.createElement("h4")
+        songTitle.classList.add("songName");
+        songTitle.innerText = song.title;
+        songsRowElement.appendChild(songTitle);
+    });
 }
+
+const clearChildren = function (element) {
+    while(element.firstChild){
+        element.removeChild(element.lastChild);
+    }
+}
+    
+
 export{
     displaySingleAlbum,
-    // clearChildren
+    clearChildren
 }
+/*    <div class="row song-row">
+        <div class="image">
+             <img class="play-song" src="./spec/Img/icons8-play-64.png" alt="Avatar">
+         </div>
+         <div class="song song-info">
+             <!-- link to song name -->
+             <h4><b>Song Name</b></h4>
+             <!-- add runtime here -->
+             <p>Duration</p>
+         </div> */
+//      </div>
+            {/* <div class="single-album">
+            <div class="album-card">
+                <img class="single-album-image" src="./spec/Img/milkshake.PNG" alt="Avatar">
+                <div class="album-info">
+                    <!-- need to link to single album name -->
+                    <h4><b>Album Name</b></h4>
+                    <!-- need to link to artist name -->
+                    <p>Artist Name</p>
+                    <p>Rating:</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                <label for="subject">Add a comment about your favorite song...</label>
+                </div>
+                <div class="col-75">
+                <textarea id="subject" name="subject" placeholder="Write a comment.." style="height:200px"></textarea>
+                </div>
+            </div>
+            </div> */}
+
